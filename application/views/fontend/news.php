ca3436c5c2d70ef2e24bbb82475a7d911a9440f5
benzintel <benz20003@gmail.com>
2016-01-23 16:52:11 +0700

    Fix news select data news To pages news-detail.php

--- application/views/fontend/news.php
+++ application/views/fontend/news.php
@@ -5,25 +5,27 @@
         <div class="text-center"><?= $alert ?></div>
     <? } ?>
 
-    <? foreach ($news as $data) { ?>
-        <div class="news-item">
-            <div class="col-md-5">
-                <a href="index.php/welcome/news/?news=<?php echo $data['news_id']; ?>">
-                    <img src="upload/<?= $data['news_pic']; ?>" alt="">
-                </a>
-            </div>
-
-            <div class="news-item--ct col-md-7">
-                <h3 class="news-item--hd">
-                    <a href="index.php/welcome/news/?news=<?php echo $data['news_id']; ?>">
-                        <?= $data['news_name']; ?>
+    <? foreach ($news as $key => $data) {
+        if ($key <= 8) { ?>
+            <div class="news-item">
+                <div class="col-md-5">
+                    <a href="index.php/welcome/news_detail/<?php echo $data['news_id']; ?>">
+                        <img src="upload/<?= $data['news_pic']; ?>" alt="">
                     </a>
-                </h3>
+                </div>
 
-                <p class="news-item--dt">
-                    <?= $data['news_detail']; ?>
-                </p>
+                <div class="news-item--ct col-md-7">
+                    <h3 class="news-item--hd">
+                        <a href="index.php/welcome/news_detail/<?php echo $data['news_id']; ?>">
+                            <?= $data['news_name']; ?>
+                        </a>
+                    </h3>
+
+                    <p class="news-item--dt">
+                        <?= $data['news_detail']; ?>
+                    </p>
+                </div>
             </div>
-        </div>
-    <? } ?>
+        <? }
+    }?>
 </div>
