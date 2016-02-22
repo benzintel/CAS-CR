c31652be190efa216cb2cff0e01c186e3a288ae7
KannipaS <sr.kannipa_st@tni.ac.th>
2016-02-22 10:54:16 +0700

    index-update

--- application/views/fontend/about_us.php
+++ application/views/fontend/about_us.php
@@ -1,29 +1,18 @@
-<div class="content-header"<h2>About us</h2></div><br>
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CAS-CR was established in  7 March 1994, that was known as the Authorized Distributor of Wire Processing  Machine and Raw Material which use in  wire harness industrial.
-    &nbsp;We are ability to serve you present and future requirement is realized through stock keeping at ware house in Thailand to guaranty that the customer always request the part upon there’s schedule.
-    &nbsp;And we have  good service both  before and after purchase by  professional local engineering  which have long experience.
-    <br>&nbsp;&nbsp;Inclusive can support you to find good solutions and suggest   about any technical for maintenance or guidance to decide your  new machine as specification requirement.</p>
-<div class="section-header">OUR Maker and Main Customer</div>
-<div class="cust">
-    <img src="img/logo/kodera.png">
-    <img src="img/logo/ts.jpg">
-    <img src="img/logo/km.png">
-    <img src="img/logo/mcm.png">
-    <img src="img/logo/stp.jpg">
-    <img src="img/logo/ikei.jpg">
-    <img src="img/logo/jst.jpg">
-    <img src="img/logo/cyg.jpg">
+<div class="content-header"><h2>About us</h2></div><br>
+<div class="about-content">
+    <p>CAS-CR was established in 7 March 1994, that was known as the Authorized Distributor of Wire Processing Machine
+        and Raw Material which use in wire harness industrial.
+        We are ability to serve you present and future requirement is realized through stock keeping at ware house in
+        Thailand to guaranty that the customer always request the part upon there’s schedule.
+        And we have good service both before and after purchase by professional local engineering which have long
+        experience.</p>
+
+    <p>Inclusive can support you to find good solutions and suggest about any technical for maintenance or guidance to
+        decide your new machine as specification requirement.</p>
 </div>
-<div class="section-header">Main Customer</div>
-<div class="cust">
-    <img src="img/logo/min.png">
-    <img src="img/logo/smtm.png">
-    <img src="img/logo/tap.jpg">
-    <img src="img/logo/fuj.png">
-    <img src="img/logo/toshi.png">
-    <img src="img/logo/lear.png">
-</div>
+
 <div class="section-header">Our map</div>
+
 <div class="img-center">
-    <img src="img/map.png" />
+    <img src="img/map.png"/>
 </div>
